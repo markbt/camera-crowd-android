17e0456bb4291619320b42e5dcee3890f7fb9007
Mark Thomas <markbt@efaref.net>
2015-06-27 20:26:01 +0100

    Initial Import

---
+++ app/src/main/java/net/efaref/cameracrowd/CameraPreview.java
@@ -0,0 +1,114 @@
+package net.efaref.cameracrowd;
+
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.FrameLayout;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+    private FrameLayout mFrame;
+
+    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.05;
+        double targetRatio = (double) w/h;
+
+        if (sizes==null) return null;
+
+        Size optimalSize = null;
+
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        // Find size
+        for (Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+    public CameraPreview(Context context, Camera camera, FrameLayout frame) {
+        super(context);
+        mCamera = camera;
+        mFrame = frame;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+
+        try {
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            Log.d("CameraCrowd", "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d("CameraCrowd", "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void cameraReleased() {
+        mCamera = null;
+    }
+}
---
+++ app/src/main/java/net/efaref/cameracrowd/CaptureActivity.java
@@ -0,0 +1,166 @@
+package net.efaref.cameracrowd;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+
+
+public class CaptureActivity extends ActionBarActivity {
+
+    Camera mCamera;
+    CameraPreview mPreview;
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(final byte[] data, Camera camera) {
+            new AsyncTask() {
+                @Override
+                protected Object doInBackground(Object[] params) {
+                    HttpClient client = new DefaultHttpClient();
+                    HttpPost post = new HttpPost("http://212.13.201.166:10080/add");
+                    try
+
+                    {
+                        StringEntity se = new StringEntity(Base64.encodeToString(data, Base64.DEFAULT));
+                        post.setEntity(se);
+                        client.execute(post);
+                    }
+
+                    catch(
+                    Exception e
+                    )
+
+                    {
+                        Log.e("CameraCrowd", "Unexpected Exception", e);
+                    }
+                    return null;
+                }
+
+            }.execute();
+            mCamera.startPreview();
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        FrameLayout mFrame = (FrameLayout) findViewById(R.id.camera_preview);
+        mPreview = new CameraPreview(this, mCamera, mFrame);
+
+        mFrame.addView(mPreview);
+
+        Camera.Size size = mCamera.getParameters().getPreviewSize();
+
+        //landscape
+        float ratio = (float)size.width/size.height;
+
+        //portrait
+        //float ratio = (float)size.height/size.width;
+
+        int new_width=0, new_height=0;
+        if(mFrame.getWidth()/mFrame.getHeight()<ratio){
+            new_width = Math.round(mFrame.getHeight()*ratio);
+            new_height = mFrame.getHeight();
+        }else{
+            new_width = mFrame.getWidth();
+            new_height = Math.round(mFrame.getWidth()/ratio);
+        }
+        mFrame.setLayoutParams(new FrameLayout.LayoutParams(new_width, new_height));
+        Log.d("=======MBT==>", "new:" + new_width + " x " + new_height);
+
+
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return false;
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+            mPreview.cameraReleased();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
